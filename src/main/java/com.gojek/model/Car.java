11b1bf1cbdf4bfd588f0f150bf152a149e09b7ad
Abhishek Jasiwal <abhishek.jaiswal@myntra.com>
2019-08-14 13:31:44 +0530

    Adding Casr object

---
+++ src/main/java/com.gojek/model/Car.java
@@ -0,0 +1,38 @@
+package com.gojek.model;
+
+
+public class Car {
+    String regNo;
+    String color;
+
+    public Car(String regNo, String color) {
+        this.regNo = regNo;
+        this.color = color;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Car{" +
+                "regNo='" + regNo + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
+
+

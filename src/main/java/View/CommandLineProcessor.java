943302cfcfc35e0e4b66cdc784f2ad5bac6dc880
Abhishek Jasiwal <abhishek.jaiswal@myntra.com>
2019-08-14 15:43:31 +0530

    processing file and interactive command

--- src/main/java/View/CommandLineProcessor.java
+++ src/main/java/View/CommandLineProcessor.java
@@ -1,4 +1,20 @@
 package View;
 
-public class CommandLineProcessor {
+import controller.Processor;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CommandLineProcessor extends Processor {
+
+    public void process() throws Exception {
+        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true) {
+            String inputString = bufferRead.readLine();
+            validateAndProcess(inputString);
+        }
+    }
+
 }
--- src/main/java/View/FileProcessor.java
+++ src/main/java/View/FileProcessor.java
@@ -3,16 +3,25 @@ package View;
 import controller.Processor;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class FileProcessor  extends Processor {
+    String filePath = null;
+
+    public FileProcessor(String filePath) {
+        this.filePath = filePath;
+    }
+
     public void process() throws Exception {
-        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-
-
-        while(true) {
-            String inputString = bufferRead.readLine();
-            validateAndProcess(inputString);
+        File inputFile = new File(filePath);
+        @SuppressWarnings("resource")
+        BufferedReader br = new BufferedReader(new FileReader(inputFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+            validateAndProcess(line);
         }
     }
+
 }

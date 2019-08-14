7e4c2c456f0f51af5cee77ca7f4d444e0a9928d0
Abhishek Jasiwal <abhishek.jaiswal@myntra.com>
2019-08-14 15:45:05 +0530

    main method to run the program

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import View.CommandLineProcessor;
+import View.FileProcessor;
+import controller.Processor;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Processor processor = null;
+
+        if(args.length >= 1) {
+            processor = new FileProcessor(args[0]);
+        } else {
+            processor = new CommandLineProcessor();
+        }
+        processor.process();
+    }
+
+}
